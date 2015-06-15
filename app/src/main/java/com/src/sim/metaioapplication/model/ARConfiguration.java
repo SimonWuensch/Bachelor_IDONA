1b4f1b7c72d7156c6f51c3f50abcf04b3595eeea
Simon Wuensch <wuensch.simon@gmail.com>
2015-06-15 13:51:34 +0200

    some changes

--- app/src/main/java/com/src/sim/metaioapplication/model/ARConfiguration.java
+++ app/src/main/java/com/src/sim/metaioapplication/model/ARConfiguration.java
@@ -15,6 +15,8 @@ import com.metaio.sdk.jni.Vector4d;
 import com.metaio.tools.io.AssetsManager;
 import com.src.sim.metaioapplication.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -23,12 +25,15 @@ import java.util.Random;
 public class ARConfiguration extends ARViewActivity{
 
     private String mTrackingFile;
-    private IGeometry arrowOne;
-    private IGeometry arrowTwo;
-    private IGeometry arrowThree;
-    private IGeometry arrowFour;
-    private IGeometry arrowFive;
-    private IGeometry arrowSix;
+    //0, 0, 1, 1.6 oben
+    //rechts 1, 0, 0, 0
+
+
+    private List<IGeometry> geometries;
+    float x = 0f;
+    float y = 0f;
+    float z = 0f;
+
     @Override
     protected int getGUILayout() {
         return R.layout.ar_view;
@@ -41,25 +46,25 @@ public class ARConfiguration extends ARViewActivity{
 
     @Override
     protected void loadContents() {
+        geometries = new ArrayList<>();
         mTrackingFile = AssetsManager.getAssetPath(getBaseContext(), "Assets2/TrackingData_MarkerlessFast.xml");
 
         boolean result = metaioSDK.setTrackingConfiguration(mTrackingFile);
+
         MetaioDebug.log("Tracking data loaded: " + result);
         String modelPath = AssetsManager.getAssetPath(getBaseContext(), "Assets2/arrow.md2");
 
-        loadGeometry(modelPath, arrowOne, 1);
-        loadGeometry(modelPath, arrowTwo, 2);
-        loadGeometry(modelPath, arrowThree, 3);
-        loadGeometry(modelPath, arrowFour, 4);
-        loadGeometry(modelPath, arrowFive, 5);
-        loadGeometry(modelPath, arrowSix, 6);
+        geometries.add(loadGeometry(modelPath, 1));
+        geometries.add(loadGeometry(modelPath, 2));
+        geometries.add(loadGeometry(modelPath, 3));
+        geometries.add(loadGeometry(modelPath, 4));
     }
 
-    private void loadGeometry(String modelPath, IGeometry geometry, int systemID){
+    private IGeometry loadGeometry(String modelPath, int systemID){
         if(modelPath != null){
-            geometry = metaioSDK.createGeometry(modelPath);
+            IGeometry geometry = metaioSDK.createGeometry(modelPath);
             if(geometry != null){
-                geometry.setScale(new Vector3d(0.25f, 0.25f, 0.25f));
+                geometry.setScale(new Vector3d(0.1f, 0.1f, 0.1f));
                 geometry.setVisible(true);
                 geometry.setCoordinateSystemID(systemID);
                 MetaioDebug.log("Loaded geometry " + modelPath);
@@ -67,11 +72,85 @@ public class ARConfiguration extends ARViewActivity{
             }else{
                 MetaioDebug.log(Log.ERROR, "Error loading geometry " + modelPath);
             }
+            return geometry;
         }
+        return null;
     }
 
     @Override
     protected void onGeometryTouched(IGeometry geometry) {
 
+    }
+
+    public void rotateX(View view){
+        for(IGeometry geometry : geometries){
+            if(geometry.isVisible()){
+                x += 0.01f;
+                geometry.setRotation(new Rotation(x, y, z));
+                Log.d(ARConfiguration.class.getSimpleName(), "Rotation X " + geometry.getRotation().getAxisAngle());
+            }
+        }
+    }
+
+    public void rotateY(View view){
+        for(IGeometry geometry : geometries){
+            if(geometry.isVisible()){
+                y += 0.01f;
+                geometry.setRotation(new Rotation(x, y, z));
+                Log.d(ARConfiguration.class.getSimpleName(), "Rotation Y " + geometry.getRotation().getAxisAngle());
+            }
+        }
+    }
+
+    public void rotateZ(View view){
+        for(IGeometry geometry : geometries){
+            if(geometry.isVisible()){
+                z += 0.01f;
+                geometry.setRotation(new Rotation(x, y, z));
+                Log.d(ARConfiguration.class.getSimpleName(), "Rotation Z "  + geometry.getRotation().getAxisAngle());
+            }
+        }
+    }
+
+    public void minRotateY(View view){
+        for(IGeometry geometry : geometries){
+            if(geometry.isVisible()){
+                y -= 0.01f;
+                geometry.setRotation(new Rotation(x, y, z));
+                Log.d(ARConfiguration.class.getSimpleName(), "Rotation Z "  + geometry.getRotation().getAxisAngle());
+            }
+        }
+    }
+
+    public void minRotateX(View view){
+        for(IGeometry geometry : geometries){
+            if(geometry.isVisible()){
+                x -= 0.01f;
+                geometry.setRotation(new Rotation(x, y, z));
+                Log.d(ARConfiguration.class.getSimpleName(), "Rotation Z "  + geometry.getRotation().getAxisAngle());
+            }
+        }
+    }
+
+    public void minRotateZ(View view){
+        for(IGeometry geometry : geometries){
+            if(geometry.isVisible()){
+                z -= 0.01f;
+                geometry.setRotation(new Rotation(x, y, z));
+                Log.d(ARConfiguration.class.getSimpleName(), "Rotation Z " + geometry.getCoordinateSystemID() + " - "  + geometry.getRotation().getAxisAngle());
+            }
+        }
+    }
+
+    public void zero(View view){
+        for(IGeometry geometry : geometries){
+            if(geometry.isVisible()){
+                x = 0;
+                y = 0;
+                z = 0;
+                geometry.setRotation(new Rotation(x, y, z));
+                Log.d(ARConfiguration.class.getSimpleName(), "Rotation Z "  + geometry.getRotation().getAxisAngle());
+            }
+        }
     }
 }
